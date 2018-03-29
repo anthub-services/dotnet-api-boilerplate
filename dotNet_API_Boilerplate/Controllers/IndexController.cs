4f8a60bf87157f2508722c04bb7b7580c5806474
Ricky Hurtado <odatruh.ykcir@gmail.com>
2018-03-29 20:16:27 +1300

    Init files

---
+++ dotNet_API_Boilerplate/Controllers/IndexController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNet_API_Boilerplate.Controllers
+{
+    [Route("[controller]")]
+    public class IndexController : Controller
+    {
+        // GET: index
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome to dotNet API Boilerplate!";
+        }
+    }
+}
